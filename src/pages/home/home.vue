2f41da17c30f85d2d40a67fae1d2a52371c21541
kete <ketenet@163.com>
2019-08-28 14:01:35 +0800

    first commit

---
+++ src/pages/home/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <router-link to="/list">列表页</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style scoped>
+  
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/pages/home/home.vue'
+import List from '@/pages/list/list.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
+    }
+  ]
+})
