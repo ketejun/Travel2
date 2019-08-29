53f8b47f35c43a5f653ab105e9613218e725d7f4
kete <ketenet@163.com>
2019-08-29 15:23:39 +0800

    add city-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/home.vue'
+import City from '@/pages/city/City.vue'
 
 Vue.use(Router)
 
@@ -15,6 +16,11 @@ export default new Router({
       path: '/home',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/city',
+      name: 'City',
+      component: City
     }
   ]
 })
