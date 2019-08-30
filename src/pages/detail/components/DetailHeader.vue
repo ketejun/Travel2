792763b5329bc5954377e19936e3de2e2e177d15
kete <ketenet@163.com>
2019-08-30 20:41:47 +0800

    fix problem

--- src/pages/detail/components/DetailHeader.vue
+++ src/pages/detail/components/DetailHeader.vue
@@ -43,7 +43,10 @@ export default {
     }
   },
   activated () {
-    window.addEventListener('scroll',this.handleScroll)
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
